bdd466fb968da561cc758e6477803c84d68b31cb
Roshan <thiruroshan27@gmail.com>
2025-02-15 15:11:41 +0530

    updated register sceen

--- ShoppingSysIOT/app/(tabs)/RegisterScreen.jsx
+++ ShoppingSysIOT/app/(tabs)/RegisterScreen.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
 
-export default function RegisterScreen () {
+import React, { useState } from 'react';
+import { Button, Text,TextInput, View } from 'react-native';
+
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import auth from '../../components/services/Firebaseauth'
+export default function RegisterScreen () { 
+  const [email,setEmail]= useState('') 
+  const [password,setPassword]= useState('') 
+
+  const handleRegister = () => {
+    console.log(email,password);
+    createUserWithEmailAndPassword(auth,email,password)
+    .then((userCredential) => {
+   
+      const user = userCredential.user;
+      console.log(user);
+      
+      // ...
+    })
+    
+  }
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontSize:20,fontWeight:'bold'}}>Register</Text>
+          <TextInput
+          onChangeText={setEmail}
+            placeholder="Email"
+            style={{width: 200,
+               height: 40, 
+               borderColor: 'gray', 
+               borderWidth: 1,
+               marginBottom: 10,
+               marginTop: 10,
+              paddingHorizontal: 10,}}
+            ></TextInput>
+            <TextInput 
+            onChangeText={setPassword}
+            placeholder="Password"
+            style={{width: 200, height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, paddingHorizontal: 10,}}
+            ></TextInput>
+            <Button title="Register" onPress={handleRegister}></Button>
+            <Text style={{marginVertical:10}}>Already have an account? Login</Text>
+        </View>
     );
 };
 
